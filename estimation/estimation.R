a8c3c6bae3a10709853a39c76d5ff04e68e67cb2
anhng91 <ymmein@gmail.com>
2024-06-05 23:51:20 -0400

    add data generating files

--- estimation/estimation.R
+++ estimation/estimation.R
@@ -373,29 +373,35 @@ compute_inner_loop = function(x_stheta, return_result=FALSE, estimate_theta=TRUE
       return(NA)
     }
     
-    sd = exp(x_r[length(x_r)-1])
+    sd_r = exp(x_r[length(x_r)-1])
     correlation = x_r[length(x_r)]
     mean_vec = rep(X_hh_theta_r %*% x_r[1:(length(x_r)-2)], each = n_halton_at_r) + correlation * hh_theta
-    prob = pnorm((root_r -mean_vec)/sd)
+    prob = pnorm((root_r -mean_vec)/sd_r)
     prob[which(root_r < 0)] = 0
-    prob[which(root_r >= 0)] = (prob[which(root_r >= 0)] -pnorm(- mean_vec[which(root_r >= 0)]/sd)) /(1 - pnorm(- mean_vec[which(root_r >= 0)]/sd))
-    prob[which(is.na(prob))] = 1;
+    prob[which(root_r >= 0)] = (prob[which(root_r >= 0)] -pnorm(- mean_vec[which(root_r >= 0)]/sd_r)) /(1 - pnorm(- mean_vec[which(root_r >= 0)]/sd_r))
+    prob[which(is.na(prob))] = 0;
 
-    # output = - sum(log(matrix(prob, nrow = n_halton_at_r) %>% colMeans + 1e-20) * full_insurance_indicator + log(matrix(1 - prob, nrow = n_halton_at_r) %>% colMeans + 1e-20) * (1 - full_insurance_indicator))    
+    
     predicted_prob = (matrix((1 - prob),nrow=n_halton_at_r) %>% colMeans); 
-    relevant_index = which(predicted_prob < 1 & predicted_prob > 0)
+    # relevant_index = which(predicted_prob < 1 & predicted_prob > 0)
+    relevant_index = 1:length(predicted_prob)
 
-    output = sum((full_insurance_indicator[relevant_index] - (predicted_prob[relevant_index]))^2 * mat_Y_rtheta[relevant_index]^2) + 
-      sum((full_insurance_indicator[relevant_index] - (predicted_prob[relevant_index]))^2 * mean_theta_R[relevant_index]^2) +
-      sum((full_insurance_indicator[relevant_index] - (predicted_prob[relevant_index]))^2 * min_theta_R[relevant_index]^2) + 
-      sum((full_insurance_indicator[relevant_index] - (predicted_prob[relevant_index]))^2 * max_theta_R[relevant_index]^2) + 
-      sum((full_insurance_indicator[relevant_index] - (predicted_prob[relevant_index]))^2 * n_involuntary[relevant_index]^2)
+    # output = sum((full_insurance_indicator[relevant_index] - (predicted_prob[relevant_index]))^2 * mat_Y_rtheta[relevant_index]^2) + 
+    #   sum((full_insurance_indicator[relevant_index] - (predicted_prob[relevant_index]))^2 * mean_theta_R[relevant_index]^2) +
+    #   sum((full_insurance_indicator[relevant_index] - (predicted_prob[relevant_index]))^2 * min_theta_R[relevant_index]^2) + 
+    #   sum((full_insurance_indicator[relevant_index] - (predicted_prob[relevant_index]))^2 * max_theta_R[relevant_index]^2) + 
+    #   sum((full_insurance_indicator[relevant_index] - (predicted_prob[relevant_index]))^2 * n_involuntary[relevant_index]^2)
+  
+
+    output = 
+      sum((full_insurance_indicator[relevant_index] - (predicted_prob[relevant_index]))^2)
 
     if (!silent) {
       print('actual insurance = '); print(summary(full_insurance_indicator))
       print('predicted insurance '); print(summary(predicted_prob))
       print('x_r = '); print(x_r);
       print('output = '); print(output);
+      print(summary(lm(full_insurance_indicator ~ predicted_prob))) 
     }
 
     if (is.nan(output) | is.infinite(output)) {
@@ -410,7 +416,7 @@ compute_inner_loop = function(x_stheta, return_result=FALSE, estimate_theta=TRUE
   if (is.nan(init_val) | is.na(init_val)) {
     return(NA)
   }
-  optim_r = optim(rep(0,length(x_transform[[2]]$beta_r) + 2), function(x) fx_r(x, silent=TRUE), control=list(maxit=1000), method='Nelder-Mead') 
+  optim_r = optim(rep(0,length(x_transform[[2]]$beta_r) + 2), function(x) fx_r(x, silent=TRUE), control=list(maxit=1000), method='BFGS') 
 
   fx_r(optim_r$par, silent=FALSE)
 
@@ -466,7 +472,8 @@ estimate_r_thetabar = optimize(function(xs) {
 }, c(-6,1)) 
 
 
-param_trial = compute_inner_loop(estimate_r_thetabar$minimum, return_result=TRUE, estimate_theta=TRUE, estimate_pref = TRUE)
+# param_trial = compute_inner_loop(estimate_r_thetabar$minimum, return_result=TRUE, estimate_theta=TRUE, estimate_pref = TRUE)
+param_trial = compute_inner_loop(-1, return_result=TRUE, estimate_theta=TRUE, estimate_pref = TRUE)
 
 message('computing final param_trial')
 
@@ -482,8 +489,11 @@ if (dir.exists('../../householdbundling_estimate')) {
   saveRDS(param_final, file=paste0('../../householdbundling_estimate/estimate_',job_index,'.rds'))
 }
 
+param = param_final 
 transform_param_final = transform_param(param_final$other)
 
+chim = function(just_r) {
+  transform_param_final$beta_r = just_r
 if (Sys.info()[['sysname']] == 'Windows') {
   clusterExport(cl, c('transform_param_final', 'param','counterfactual_household_draw_theta_kappa_Rdraw'))
   fit_values = parLapply(cl, sample_r_theta, function(id) {
@@ -519,4 +529,6 @@ actual_data_summary$type = 'actual'
 
 plot_1 = ggplot(data = rbind(predicted_data_summary, actual_data_summary), aes(x = Y2, y = mean_Vol_sts, color=type)) + geom_line() 
 plot_2 = ggplot(data = rbind(predicted_data_summary, actual_data_summary), aes(x = Y2, y = mean_m, color=type)) + geom_line() 
-gridExtra::grid.arrange(plot_1, plot_2, nrow=1)
+plot = gridExtra::grid.arrange(plot_1, plot_2, nrow=1)
+return(plot)
+}
